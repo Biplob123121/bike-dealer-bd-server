e12a5f36c59e6efc2cf828b35f8b9fee033c2044
Biplob123121 <biplobkb123@gmail.com>
2023-03-11 23:25:17 +0600

    updated the db url

--- index.js
+++ index.js
@@ -14,7 +14,7 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
 
-const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.pgyy5.mongodb.net/?retryWrites=true&w=majority`;
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.kq1reah.mongodb.net/?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
 
 
@@ -38,7 +38,7 @@ function verifyJWT(req, res, next) {
 async function run() {
   try {
     await client.connect();
-    const productCollection = client.db('bikeDealerBD').collection('product');
+    const productCollection = client.db('bike_dealer_BD').collection('product');
 
     //JWT
     app.post('/login', async (req, res) => {
